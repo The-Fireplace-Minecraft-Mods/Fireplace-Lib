8cc80e5b297aff19ed848da0864a6be96d73d429
The_Fireplace <the.f1repl4ce@gmail.com>
2021-01-28 00:34:47 -0600

    Switch to a more efficient way to create the empty UUID

--- src/main/java/the_fireplace/lib/api/util/EmptyUUID.java
+++ src/main/java/the_fireplace/lib/api/util/EmptyUUID.java
@@ -3,7 +3,7 @@ package the_fireplace.lib.api.util;
 import java.util.UUID;
 
 public final class EmptyUUID {
-    public static final UUID EMPTY_UUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
+    public static final UUID EMPTY_UUID = new UUID(0, 0);
 
     public static boolean isEmpty(UUID uuid) {
         return EMPTY_UUID.equals(uuid);
