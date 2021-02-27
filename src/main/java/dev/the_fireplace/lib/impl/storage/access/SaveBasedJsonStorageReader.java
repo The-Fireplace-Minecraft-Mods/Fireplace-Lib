21d710b65d3cedcc854ab3f90280d38673110619
The_Fireplace <the.f1repl4ce@gmail.com>
2021-02-27 15:26:21 -0600

    Put together a way to query storage for IDs, add proper validation and documentation to the storage APIs

--- src/main/java/dev/the_fireplace/lib/impl/storage/access/SaveBasedJsonStorageReader.java
+++ src/main/java/dev/the_fireplace/lib/impl/storage/access/SaveBasedJsonStorageReader.java
@@ -6,11 +6,20 @@ import dev.the_fireplace.lib.api.storage.SaveBasedSerializable;
 import dev.the_fireplace.lib.api.storage.access.SaveBasedStorageReader;
 import dev.the_fireplace.lib.api.storage.access.intermediary.StorageReadBuffer;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class SaveBasedJsonStorageReader implements SaveBasedStorageReader {
     @Deprecated
     public static final SaveBasedStorageReader INSTANCE = new SaveBasedJsonStorageReader();
+    @SuppressWarnings("HardcodedFileSeparator")
+    private static final Pattern JSON_FILE_REGEX = Pattern.compile('^' + SchemaValidator.SCHEMA_PATTERN_STRING + "\\.json$");
+    private static final Pattern JSON_EXTENSION_LITERAL = Pattern.compile(".json", Pattern.LITERAL);
     private final JsonFileReader fileReader;
 
     private SaveBasedJsonStorageReader() {
@@ -28,5 +37,26 @@ public final class SaveBasedJsonStorageReader implements SaveBasedStorageReader 
 
         StorageReadBuffer reader = new JsonStorageReadBuffer(obj);
         readable.readFrom(reader);
+    }
+
+    @Override
+    public Iterator<String> getStoredIdsIterator(String database, String table) {
+        Path saveDirectory = JsonStoragePath.resolveSaveBasedJsonDirectory(database, table);
+
+        File[] files = saveDirectory.toFile().listFiles((file, s) -> JSON_FILE_REGEX.matcher(s).matches());
+
+        return Arrays.stream(files).map(f ->
+            JSON_EXTENSION_LITERAL.matcher(f.getName().toLowerCase(Locale.ROOT)).replaceAll(Matcher.quoteReplacement(""))
+        ).iterator();
+    }
+
+    @Override
+    public boolean isStored(String database, String table, String id) {
+        return JsonStoragePath.resolveSaveBasedJsonFilePath(database, table, id).toFile().exists();
+    }
+
+    @Override
+    public boolean isStored(SaveBasedSerializable saveBasedSerializable) {
+        return isStored(saveBasedSerializable.getDatabase(), saveBasedSerializable.getTable(), saveBasedSerializable.getId());
     }
 }
---
+++ src/main/java/dev/the_fireplace/lib/impl/storage/access/SchemaValidator.java
@@ -0,0 +1,20 @@
+package dev.the_fireplace.lib.impl.storage.access;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class SchemaValidator {
+    @SuppressWarnings("HardcodedFileSeparator")
+    static final String SCHEMA_PATTERN_STRING = "[a-zA-Z0-9_\\-]+";
+    private static final Pattern VALID_SCHEMA_REGEX = Pattern.compile('^' + SCHEMA_PATTERN_STRING + '$');
+    private static final Pattern DASHES = Pattern.compile("-", Pattern.LITERAL);
+
+    static boolean isValid(String s) {
+        return VALID_SCHEMA_REGEX.matcher(s).matches();
+    }
+
+    static String minimizeSchema(String s) {
+        return DASHES.matcher(s).replaceAll(Matcher.quoteReplacement("")).toLowerCase(Locale.ROOT);
+    }
+}
