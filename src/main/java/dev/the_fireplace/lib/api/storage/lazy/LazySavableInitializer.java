5b1f0f669032fb2330eadadc8ee5712b4598672f
The_Fireplace <the.f1repl4ce@gmail.com>
2021-02-27 11:14:30 -0600

    Added an API function to unregister things from the save timer, and a LazyInitializer method to take care of calling it.

--- src/main/java/dev/the_fireplace/lib/api/storage/lazy/LazySavableInitializer.java
+++ src/main/java/dev/the_fireplace/lib/api/storage/lazy/LazySavableInitializer.java
@@ -6,15 +6,22 @@ public final class LazySavableInitializer {
     private static final SaveTimer TIMER = SaveTimer.getInstance();
 
     public static <T extends ThreadsafeLazySavable> T lazyInitialize(T savable, int saveIntervalInMinutes) {
-        saveIntervalInMinutes = Math.min(saveIntervalInMinutes, Short.MAX_VALUE);
         savable.load();
         if (saveIntervalInMinutes > 0) {
-            TIMER.registerSaveFunction((short) saveIntervalInMinutes, savable::save);
+            TIMER.register(boundToShort(saveIntervalInMinutes), savable::save);
         } else if (isNonDefault(savable)) {
             savable.forceSave();
         }
 
         return savable;
+    }
+
+    public static void tearDown(ThreadsafeLazySavable savable, int saveIntervalInMinutes) {
+        TIMER.unregister(boundToShort(saveIntervalInMinutes), savable::save);
+    }
+
+    private static short boundToShort(int saveIntervalInMinutes) {
+        return (short)Math.max(Short.MIN_VALUE, Math.min(Short.MAX_VALUE, saveIntervalInMinutes));
     }
 
     private static boolean isNonDefault(Object obj) {
