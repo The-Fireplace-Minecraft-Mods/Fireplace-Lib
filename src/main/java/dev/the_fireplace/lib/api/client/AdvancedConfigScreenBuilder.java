f85ceed3705956dd59733dfd4f44968beaf0f15c
The_Fireplace <the.f1repl4ce@gmail.com>
2021-05-09 18:11:36 -0500

    Fix enum dropdowns not accepting enums

--- src/main/java/dev/the_fireplace/lib/api/client/AdvancedConfigScreenBuilder.java
+++ src/main/java/dev/the_fireplace/lib/api/client/AdvancedConfigScreenBuilder.java
@@ -54,10 +54,10 @@ public abstract class AdvancedConfigScreenBuilder extends ConfigScreenBuilderBas
         ConfigEntryBuilder entryBuilder,
         ConfigCategory category,
         String optionTranslationBase,
-        Enum<T> currentValue,
-        Enum<T> defaultValue,
-        Iterable<Enum<T>> dropdownEntries,
-        Consumer<Enum<T>> saveFunction
+        T currentValue,
+        T defaultValue,
+        Iterable<T> dropdownEntries,
+        Consumer<T> saveFunction
     ) {
         return addEnumDropdown(
             entryBuilder,
@@ -75,14 +75,14 @@ public abstract class AdvancedConfigScreenBuilder extends ConfigScreenBuilderBas
         ConfigEntryBuilder entryBuilder,
         ConfigCategory category,
         String optionTranslationBase,
-        Enum<T> currentValue,
-        Enum<T> defaultValue,
-        Iterable<Enum<T>> dropdownEntries,
-        Consumer<Enum<T>> saveFunction,
+        T currentValue,
+        T defaultValue,
+        Iterable<T> dropdownEntries,
+        Consumer<T> saveFunction,
         byte descriptionRowCount
     ) {
         List<String> stringEntries = new ArrayList<>();
-        for (Enum<T> entry: dropdownEntries) {
+        for (T entry: dropdownEntries) {
             stringEntries.add(entry.toString());
         }
         return addStringDropdown(
