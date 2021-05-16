796dcb628b1091d772f0224d6fc423bbe93d3354
The_Fireplace <the.f1repl4ce@gmail.com>
2021-05-16 13:02:52 -0500

    Mark client packet receiver registry as client side only

--- src/main/java/dev/the_fireplace/lib/api/network/client/ClientPacketReceiverRegistry.java
+++ src/main/java/dev/the_fireplace/lib/api/network/client/ClientPacketReceiverRegistry.java
@@ -1,7 +1,10 @@
 package dev.the_fireplace.lib.api.network.client;
 
 import dev.the_fireplace.lib.impl.network.client.ClientPacketReceiverRegistryImpl;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public interface ClientPacketReceiverRegistry {
     static ClientPacketReceiverRegistry getInstance() {
         //noinspection deprecation
