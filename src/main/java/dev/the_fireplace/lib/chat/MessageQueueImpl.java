48a5d2549ac0de89e257e6ca1d88e8eeb4650cc8
The_Fireplace <the.f1repl4ce@gmail.com>
2021-06-23 17:37:24 -0500

    Fix common DI mistakes

--- src/main/java/dev/the_fireplace/lib/chat/MessageQueueImpl.java
+++ src/main/java/dev/the_fireplace/lib/chat/MessageQueueImpl.java
@@ -25,7 +25,7 @@ public final class MessageQueueImpl implements MessageQueue {
     private final EmptyUUID emptyUUID;
 
     @Inject
-    private MessageQueueImpl(ExecutionManager executionManager, EmptyUUID emptyUUID) {
+    public MessageQueueImpl(ExecutionManager executionManager, EmptyUUID emptyUUID) {
         this.executionManager = executionManager;
         this.emptyUUID = emptyUUID;
     }
--- src/main/java/dev/the_fireplace/lib/uuid/EmptyUUIDImpl.java
+++ src/main/java/dev/the_fireplace/lib/uuid/EmptyUUIDImpl.java
@@ -3,9 +3,11 @@ package dev.the_fireplace.lib.uuid;
 import dev.the_fireplace.annotateddi.api.di.Implementation;
 import dev.the_fireplace.lib.api.uuid.injectables.EmptyUUID;
 
+import javax.inject.Singleton;
 import java.util.UUID;
 
 @Implementation
+@Singleton
 public final class EmptyUUIDImpl implements EmptyUUID {
     private static final UUID EMPTY_UUID = new UUID(0, 0);
 
