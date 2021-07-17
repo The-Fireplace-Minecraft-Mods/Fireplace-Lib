c59029ad6b3261da4f340176f2cda5bfb9c0152f
The_Fireplace <the.f1repl4ce@gmail.com>
2021-07-17 09:38:13 -0500

    Remove direct references to Mod Menu from the Config Screen Factory

--- src/main/java/dev/the_fireplace/lib/entrypoints/ModMenuEntrypoint.java
+++ src/main/java/dev/the_fireplace/lib/entrypoints/ModMenuEntrypoint.java
@@ -3,16 +3,16 @@ package dev.the_fireplace.lib.entrypoints;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.the_fireplace.annotateddi.api.DIContainer;
-import dev.the_fireplace.lib.config.FLModMenuIntegration;
+import dev.the_fireplace.lib.config.FLConfigScreenFactory;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
 public final class ModMenuEntrypoint implements ModMenuApi {
-    private final FLModMenuIntegration flModMenuIntegration = DIContainer.get().getInstance(FLModMenuIntegration.class);
+    private final FLConfigScreenFactory flConfigScreenFactory = DIContainer.get().getInstance(FLConfigScreenFactory.class);
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return flModMenuIntegration.getModConfigScreenFactory();
+        return flConfigScreenFactory.getModConfigScreenFactory();
     }
 }
