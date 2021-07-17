d662ed6ac2d4f39340be08b1182ab6e7ac1d1edb
The_Fireplace <the.f1repl4ce@gmail.com>
2021-07-17 08:03:58 -0500

    Change FL translator language when the client language changes

---
+++ src/main/java/dev/the_fireplace/lib/mixin/LanguageManagerMixin.java
@@ -0,0 +1,20 @@
+package dev.the_fireplace.lib.mixin;
+
+import dev.the_fireplace.lib.chat.translation.ModLanguageMaps;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.resource.language.LanguageManager;
+import net.minecraft.resource.ResourceManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LanguageManager.class)
+@Environment(EnvType.CLIENT)
+public class LanguageManagerMixin {
+    @Inject(at = @At("RETURN"), method = "apply")
+    private void resetFireplaceLibLanguages(ResourceManager manager, CallbackInfo ci) {
+        ModLanguageMaps.reloadLanguage();
+    }
+}
