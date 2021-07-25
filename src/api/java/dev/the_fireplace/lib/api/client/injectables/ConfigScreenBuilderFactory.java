6dcae735ba2b9219d26319b7e6b8bcbcc2f6f85f
The_Fireplace <the.f1repl4ce@gmail.com>
2021-07-25 12:31:30 -0500

    Properly annotate client side injectables as client side only

--- src/api/java/dev/the_fireplace/lib/api/client/injectables/ConfigScreenBuilderFactory.java
+++ src/api/java/dev/the_fireplace/lib/api/client/injectables/ConfigScreenBuilderFactory.java
@@ -2,8 +2,11 @@ package dev.the_fireplace.lib.api.client.injectables;
 
 import dev.the_fireplace.lib.api.chat.interfaces.Translator;
 import dev.the_fireplace.lib.api.client.interfaces.ConfigScreenBuilder;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.Screen;
 
+@Environment(EnvType.CLIENT)
 public interface ConfigScreenBuilderFactory {
     ConfigScreenBuilder create(
         Translator translator,
