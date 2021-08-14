d8a549e7c9e4034b43f599d25bbb96d3b8abd0fc
The_Fireplace <the.f1repl4ce@gmail.com>
2021-08-14 13:45:26 -0500

    Added an argument type for possibly offline players

---
+++ src/api/java/dev/the_fireplace/lib/api/command/injectables/ArgumentTypeFactory.java
@@ -0,0 +1,12 @@
+package dev.the_fireplace.lib.api.command.injectables;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import dev.the_fireplace.lib.api.command.interfaces.OfflineSupportedPlayerArgumentType;
+import dev.the_fireplace.lib.api.command.interfaces.PossiblyOfflinePlayer;
+import net.minecraft.server.command.ServerCommandSource;
+
+public interface ArgumentTypeFactory {
+    OfflineSupportedPlayerArgumentType possiblyOfflinePlayer();
+    PossiblyOfflinePlayer getPossiblyOfflinePlayer(CommandContext<ServerCommandSource> commandContext, String string) throws CommandSyntaxException;
+}
