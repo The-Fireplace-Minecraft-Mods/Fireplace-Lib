e026b2ecebe0ddf1b12842701a16145e2538803c
The_Fireplace <the.f1repl4ce@gmail.com>
2021-08-14 13:54:04 -0500

    Fix suggestions for @ player suggestions

--- src/main/java/dev/the_fireplace/lib/command/helpers/OfflinePlayerArgumentType.java
+++ src/main/java/dev/the_fireplace/lib/command/helpers/OfflinePlayerArgumentType.java
@@ -54,7 +54,7 @@ public final class OfflinePlayerArgumentType implements OfflineSupportedPlayerAr
             StringReader reader = new StringReader(builder.getInput());
             reader.setCursor(builder.getStart());
             CommandSource commandSource = (CommandSource)context.getSource();
-            EntitySelectorReader entitySelectorReader = new EntitySelectorReader(reader, commandSource.hasPermissionLevel(2));
+            EntitySelectorReader entitySelectorReader = new EntitySelectorReader(reader);
 
             try {
                 entitySelectorReader.read();
