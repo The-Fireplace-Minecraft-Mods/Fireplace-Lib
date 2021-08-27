934d2b56386f322a5942d1b0071ab8b90ac2eb34
The_Fireplace <the.f1repl4ce@gmail.com>
2021-08-26 19:42:10 -0500

    Prototype Hierarchical Config system

---
+++ src/main/java/dev/the_fireplace/lib/lazyio/hierarchicalconfig/HierarchicalConfigManagerImpl.java
@@ -0,0 +1,119 @@
+package dev.the_fireplace.lib.lazyio.hierarchicalconfig;
+
+import dev.the_fireplace.lib.api.lazyio.injectables.ReloadableManager;
+import dev.the_fireplace.lib.api.lazyio.interfaces.HierarchicalConfig;
+import dev.the_fireplace.lib.api.lazyio.interfaces.HierarchicalConfigManager;
+import dev.the_fireplace.lib.api.lazyio.interfaces.Reloadable;
+import dev.the_fireplace.lib.entrypoints.FireplaceLib;
+import dev.the_fireplace.lib.io.access.JsonStoragePath;
+
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class HierarchicalConfigManagerImpl<T extends HierarchicalConfig> implements HierarchicalConfigManager<T> {
+
+    private final HierarchicalConfigLoader configLoader;
+    private final JsonStoragePath jsonStoragePath;
+    private final ReloadableManager reloadableManager;
+
+    private final String domain;
+    private final T defaultConfig;
+    private final Iterable<String> allowedModuleIds;
+    private final Map<String, T> modules;
+
+    public HierarchicalConfigManagerImpl(
+        String domain,
+        T defaultConfig,
+        Iterable<String> allowedModuleIds,
+        HierarchicalConfigLoader configLoader,
+        JsonStoragePath jsonStoragePath,
+        ReloadableManager reloadableManager
+    ) {
+        this.domain = domain;
+        this.defaultConfig = defaultConfig;
+        this.allowedModuleIds = allowedModuleIds;
+        this.modules = new ConcurrentHashMap<>();
+        this.configLoader = configLoader;
+        this.jsonStoragePath = jsonStoragePath;
+        this.reloadableManager = reloadableManager;
+
+        loadExistingHierarchy();
+        registerHierarchyReloadable();
+    }
+
+    private void loadExistingHierarchy() {
+        for (String id: allowedModuleIds) {
+            Path filePath = jsonStoragePath.resolveConfigBasedJsonFilePath(domain, id);
+            if (filePath.toFile().exists() && !modules.containsKey(id)) {
+                HierarchicalConfig module = defaultConfig.clone();
+                try {
+                    //noinspection unchecked
+                    addCustom(id, (T) module);
+                } catch (ClassCastException exception) {
+                    FireplaceLib.getLogger().error("Cloned hierarchical config was not of the expected type!", exception);
+                }
+            }
+        }
+    }
+
+    private void registerHierarchyReloadable() {
+        reloadableManager.register(new Reloadable() {
+            @Override
+            public void reload() {
+                loadExistingHierarchy();
+            }
+
+            @Override
+            public String getReloadGroup() {
+                return domain;
+            }
+        });
+    }
+
+    @Override
+    public Iterable<String> getAllowedModuleIds() {
+        return allowedModuleIds;
+    }
+
+    @Override
+    public T get(String moduleId) {
+        return modules.getOrDefault(moduleId, defaultConfig);
+    }
+
+    @Override
+    public boolean isCustom(String moduleId) {
+        return modules.containsKey(moduleId);
+    }
+
+    @Override
+    public void addCustom(String moduleId, T module) {
+        modules.put(moduleId, module);
+        configLoader.initialize(module, domain, moduleId);
+    }
+
+    @Override
+    public boolean deleteCustom(String moduleId) {
+        T module = modules.remove(moduleId);
+        if (module == null) {
+            return false;
+        }
+
+        return configLoader.delete(module, domain, moduleId);
+    }
+
+    public void saveAllCustoms() {
+        for (Map.Entry<String, T> moduleEntry : modules.entrySet()) {
+            configLoader.save(moduleEntry.getValue(), domain, moduleEntry.getKey());
+        }
+    }
+
+    public void saveCustom(String id) {
+        T module = modules.get(id);
+        if (module == null) {
+            FireplaceLib.getLogger().error("Custom config does not exist, and cannot be saved: " + id);
+            return;
+        }
+        configLoader.save(module, domain, id);
+    }
+}
