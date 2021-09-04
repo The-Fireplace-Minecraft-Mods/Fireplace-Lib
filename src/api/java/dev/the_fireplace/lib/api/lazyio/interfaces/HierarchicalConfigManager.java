9a939cca24bc593e0713fc649a5ad63af18f8060
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-03 19:50:49 -0500

    Add a way to get which modules are custom in the HierarchicalConfigManager

--- src/api/java/dev/the_fireplace/lib/api/lazyio/interfaces/HierarchicalConfigManager.java
+++ src/api/java/dev/the_fireplace/lib/api/lazyio/interfaces/HierarchicalConfigManager.java
@@ -1,14 +1,18 @@
 package dev.the_fireplace.lib.api.lazyio.interfaces;
 
+import java.util.Collection;
+
 public interface HierarchicalConfigManager<T extends HierarchicalConfig> {
     Iterable<String> getAllowedModuleIds();
 
     T get(String moduleId);
 
+    Collection<String> getCustoms();
+
     boolean isCustom(String moduleId);
 
     void addCustom(String moduleId, T module);
-    
+
     boolean deleteCustom(String moduleId);
 
     void saveAllCustoms();
