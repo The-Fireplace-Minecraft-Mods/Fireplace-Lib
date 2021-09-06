9208b13ae58d6ec4318f9becde3f994dc0ee217e
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-06 14:52:25 -0500

    Use MessageQueue in FeedbackSender
    Reduce likelihood of interfering with multiline messages by using MessageQueue in FeedbackSender

---
+++ src/main/java/dev/the_fireplace/lib/mixin/ServerCommandSourceAccessor.java
@@ -0,0 +1,12 @@
+package dev.the_fireplace.lib.mixin;
+
+import net.minecraft.server.command.CommandOutput;
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ServerCommandSource.class)
+public interface ServerCommandSourceAccessor {
+    @Accessor
+    CommandOutput getOutput();
+}
