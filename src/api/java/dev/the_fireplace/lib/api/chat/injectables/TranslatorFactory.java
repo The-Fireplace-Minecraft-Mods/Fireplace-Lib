26db80ba77602ba4c017dcac3ed3a83ff52c1826
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-06 14:25:50 -0500

    Improve some API documentation

--- src/api/java/dev/the_fireplace/lib/api/chat/injectables/TranslatorFactory.java
+++ src/api/java/dev/the_fireplace/lib/api/chat/injectables/TranslatorFactory.java
@@ -8,7 +8,7 @@ public interface TranslatorFactory {
     /**
      * Properly initializes your translation service.
      * This should happen on both client (If the mod is present there) and server to ensure proper synchronization.
-     * {@link net.fabricmc.api.ModInitializer#onInitialize} is a good place to do so.
+     * {@link net.fabricmc.api.ModInitializer#onInitialize}/{@link dev.the_fireplace.annotateddi.api.entrypoints.DIModInitializer#onInitialize} is a good place to do so.
      */
     void addTranslator(String modid);
 
