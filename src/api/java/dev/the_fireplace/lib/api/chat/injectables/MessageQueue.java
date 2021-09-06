37bde1f9c822a0d972eb9758d21d09246ca4a512
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-06 14:25:50 -0500

    Improve some API documentation

--- src/api/java/dev/the_fireplace/lib/api/chat/injectables/MessageQueue.java
+++ src/api/java/dev/the_fireplace/lib/api/chat/injectables/MessageQueue.java
@@ -4,5 +4,8 @@ import net.minecraft.server.command.CommandOutput;
 import net.minecraft.text.Text;
 
 public interface MessageQueue {
+    /**
+     * Synchronized message queue for sending single/multiline messages to help prevent overlap when multiple threads are sending messages at once.
+     */
     void queueMessages(CommandOutput messageTarget, Text... messages);
 }
