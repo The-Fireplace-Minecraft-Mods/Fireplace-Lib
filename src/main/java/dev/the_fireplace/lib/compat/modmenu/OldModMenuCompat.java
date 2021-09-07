4fcbb12f4d9ec9109ed6f44b538b0c91733957f2
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-06 19:54:49 -0500

    Port the old mod menu compat to 1.16.5/Java 8

--- src/main/java/dev/the_fireplace/lib/compat/modmenu/OldModMenuCompat.java
+++ src/main/java/dev/the_fireplace/lib/compat/modmenu/OldModMenuCompat.java
@@ -1,5 +1,6 @@
 package dev.the_fireplace.lib.compat.modmenu;
 
+import com.google.common.collect.Sets;
 import com.terraformersmc.modmenu.ModMenu;
 import com.terraformersmc.modmenu.gui.ModsScreen;
 import dev.the_fireplace.lib.mixin.clothconfig.AbstractConfigScreenAccessor;
@@ -9,10 +10,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 
 import java.util.Map;
-import java.util.Set;
 
 /**
- * Counteract Mod Menu's old caching mechanism (MM 2.0.2 and earlier), which shouldn't be used with Cloth Config GUIs.
+ * Counteract Mod Menu's old caching mechanism (MM 1.16.9 and earlier), which shouldn't be used with Cloth Config GUIs.
  * See also: https://github.com/TerraformersMC/ModMenu/issues/254
  */
 @Environment(EnvType.CLIENT)
@@ -20,11 +20,11 @@ public final class OldModMenuCompat implements ModMenuCompat {
     @Override
     public void reloadClothConfigGUIs() {
         Screen screen = MinecraftClient.getInstance().currentScreen;
-        if (screen instanceof AbstractConfigScreenAccessor configScreenAccessor) {
-            Screen parent = configScreenAccessor.getParent();
-            if (parent instanceof ModsScreen modsScreen) {
-                Map<String, Screen> configScreenCache = modsScreen.getConfigScreenCache();
-                for (Map.Entry<String, Screen> configScreen : Set.copyOf(configScreenCache.entrySet())) {
+        if (screen instanceof AbstractConfigScreenAccessor) {
+            Screen parent = ((AbstractConfigScreenAccessor) screen).getParent();
+            if (parent instanceof ModsScreen) {
+                Map<String, Screen> configScreenCache = ((ModsScreen) parent).getConfigScreenCache();
+                for (Map.Entry<String, Screen> configScreen : Sets.newHashSet(configScreenCache.entrySet())) {
                     boolean isClothConfigScreen = configScreen.getValue() instanceof AbstractConfigScreenAccessor;
                     if (!isClothConfigScreen) {
                         continue;
