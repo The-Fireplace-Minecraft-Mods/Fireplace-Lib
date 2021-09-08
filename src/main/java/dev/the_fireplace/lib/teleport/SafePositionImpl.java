d2f04f5a40e9e9b2bbdbc8009fa120c9e5ebf3e3
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-08 17:10:53 -0500

    Add teleporter and safe position finder

---
+++ src/main/java/dev/the_fireplace/lib/teleport/SafePositionImpl.java
@@ -0,0 +1,25 @@
+package dev.the_fireplace.lib.teleport;
+
+import dev.the_fireplace.annotateddi.api.di.Implementation;
+import dev.the_fireplace.lib.api.teleport.injectables.SafePosition;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.RespawnAnchorBlock;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.CollisionView;
+
+import java.util.Optional;
+
+@Implementation
+public final class SafePositionImpl implements SafePosition {
+    @Override
+    public Optional<Vec3d> findBy(EntityType<?> entityType, CollisionView world, BlockPos blockPos) {
+        return RespawnAnchorBlock.findRespawnPosition(entityType, world, blockPos);
+    }
+
+    @Override
+    public boolean canSpawnInside(EntityType<?> entityType, BlockState state) {
+        return !entityType.isInvalidSpawn(state);
+    }
+}
