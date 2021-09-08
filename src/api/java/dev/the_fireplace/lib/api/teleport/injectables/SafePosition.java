1d07f9e8dc57deb2254e962c987912e658008d11
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-08 17:10:53 -0500

    Add teleporter and safe position finder

---
+++ src/api/java/dev/the_fireplace/lib/api/teleport/injectables/SafePosition.java
@@ -0,0 +1,15 @@
+package dev.the_fireplace.lib.api.teleport.injectables;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.CollisionView;
+
+import java.util.Optional;
+
+public interface SafePosition {
+    Optional<Vec3d> findBy(EntityType<?> entityType, CollisionView world, BlockPos blockPos);
+
+    boolean canSpawnInside(EntityType<?> entityType, BlockState state);
+}
---
+++ src/main/java/dev/the_fireplace/lib/teleport/TeleporterImpl.java
@@ -0,0 +1,42 @@
+package dev.the_fireplace.lib.teleport;
+
+import dev.the_fireplace.annotateddi.api.di.Implementation;
+import dev.the_fireplace.lib.api.teleport.injectables.Teleporter;
+import dev.the_fireplace.lib.entrypoints.FireplaceLib;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkSectionPos;
+import net.minecraft.world.chunk.ChunkStatus;
+
+@Implementation
+public final class TeleporterImpl implements Teleporter {
+    @Override
+    public Entity teleport(Entity entity, ServerWorld targetWorld, BlockPos targetPos) {
+        return teleport(entity, targetWorld, targetPos.getX(), targetPos.getY(), targetPos.getZ());
+    }
+
+    @Override
+    public Entity teleport(Entity entity, ServerWorld targetWorld, double targetX, double targetY, double targetZ) {
+        preloadTargetChunk(targetWorld, targetX, targetZ);
+        if (entity instanceof ServerPlayerEntity) {
+            ((ServerPlayerEntity) entity).teleport(targetWorld, targetX, targetY, targetZ, entity.getYaw(), entity.getPitch());
+            return entity;
+        }
+        Entity entityInTargetWorld = entity.moveToWorld(targetWorld);
+        if (entityInTargetWorld != null) {
+            entityInTargetWorld.setPosition(targetX, targetY, targetZ);
+            return entityInTargetWorld;
+        } else {
+            FireplaceLib.getLogger().warn("Entity was removed before it could be moved to target world.", new Exception("Stack Trace"));
+            return entity;
+        }
+    }
+
+    private void preloadTargetChunk(ServerWorld targetWorld, double targetBlockX, double targetBlockZ) {
+        int chunkX = ChunkSectionPos.getSectionCoord(targetBlockX);
+        int chunkZ = ChunkSectionPos.getSectionCoord(targetBlockZ);
+        targetWorld.getChunk(chunkX, chunkZ, ChunkStatus.FULL, true);
+    }
+}
