19027e2e12560b1ccc6aecbc48d16b9d123de593
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-25 10:40:31 -0500

    Add ability to have a custom screen to build config screen options

--- src/main/java/dev/the_fireplace/lib/config/FLConfigScreenFactory.java
+++ src/main/java/dev/the_fireplace/lib/config/FLConfigScreenFactory.java
@@ -1,10 +1,14 @@
 package dev.the_fireplace.lib.config;
 
+import dev.the_fireplace.lib.api.chat.injectables.TextStyles;
 import dev.the_fireplace.lib.api.chat.injectables.TranslatorFactory;
 import dev.the_fireplace.lib.api.chat.interfaces.Translator;
 import dev.the_fireplace.lib.api.client.injectables.ConfigScreenBuilderFactory;
 import dev.the_fireplace.lib.api.client.interfaces.ConfigScreenBuilder;
+import dev.the_fireplace.lib.api.client.interfaces.CustomButtonScreen;
+import dev.the_fireplace.lib.api.client.interfaces.CustomButtonScreenFactory;
 import dev.the_fireplace.lib.api.lazyio.injectables.ConfigStateManager;
+import dev.the_fireplace.lib.config.cloth.test.TestCustomButtonScreen;
 import dev.the_fireplace.lib.domain.config.ConfigValues;
 import dev.the_fireplace.lib.entrypoints.FireplaceLib;
 import net.fabricmc.api.EnvType;
@@ -12,6 +16,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.resource.language.LanguageDefinition;
+import net.minecraft.text.LiteralText;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -29,6 +34,7 @@ public final class FLConfigScreenFactory {
     private final FLConfig config;
     private final ConfigValues defaultConfigValues;
     private final ConfigScreenBuilderFactory configScreenBuilderFactory;
+    private final TextStyles textStyles;
 
     private ConfigScreenBuilder configScreenBuilder;
 
@@ -38,13 +44,15 @@ public final class FLConfigScreenFactory {
         ConfigStateManager configStateManager,
         FLConfig config,
         @Named("default") ConfigValues defaultConfigValues,
-        ConfigScreenBuilderFactory configScreenBuilderFactory
+        ConfigScreenBuilderFactory configScreenBuilderFactory,
+        TextStyles textStyles
     ) {
         this.translator = translatorFactory.getTranslator(FireplaceLib.MODID);
         this.configStateManager = configStateManager;
         this.config = config;
         this.defaultConfigValues = defaultConfigValues;
         this.configScreenBuilderFactory = configScreenBuilderFactory;
+        this.textStyles = textStyles;
     }
 
     public Screen getConfigScreen(Screen parent) {
@@ -56,6 +64,9 @@ public final class FLConfigScreenFactory {
             () -> configStateManager.save(config)
         );
         addGlobalCategoryEntries();
+        if (FireplaceLib.isDevelopmentEnvironment()) {
+            addDeveloperEntries();
+        }
 
         return this.configScreenBuilder.build();
     }
@@ -80,5 +91,22 @@ public final class FLConfigScreenFactory {
             defaultConfigValues.getNonEssentialThreadPoolSize(),
             config::setNonEssentialThreadPoolSize
         ).setMinimum((short) 1);
+    }
+
+    private void addDeveloperEntries() {
+        configScreenBuilder.addCustomOptionButton(
+            "Custom Button Test",
+            "Null",
+            "Null",
+            value -> {
+            },
+            new CustomButtonScreenFactory<>() {
+                @Override
+                public <T extends Screen & CustomButtonScreen<String>> T createScreen(Screen parent, String currentValue) {
+                    //noinspection unchecked
+                    return (T) new TestCustomButtonScreen(parent, currentValue);
+                }
+            }
+        ).setButtonTextSupplier(value -> new LiteralText(value).setStyle(textStyles.gold()));
     }
 }
