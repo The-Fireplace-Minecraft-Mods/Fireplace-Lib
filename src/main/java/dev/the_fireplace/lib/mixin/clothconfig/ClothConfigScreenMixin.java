fadcef3aa01098becd17b930c35bf03e4684056c
The_Fireplace <the.f1repl4ce@gmail.com>
2021-11-06 13:45:12 -0500

    Fix the mixin failing because it relies on cloth fields that can't be found

--- src/main/java/dev/the_fireplace/lib/mixin/clothconfig/ClothConfigScreenMixin.java
+++ src/main/java/dev/the_fireplace/lib/mixin/clothconfig/ClothConfigScreenMixin.java
@@ -1,26 +1,27 @@
 package dev.the_fireplace.lib.mixin.clothconfig;
 
 import me.shedaniel.clothconfig2.gui.ClothConfigScreen;
-import me.shedaniel.math.Rectangle;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ClothConfigScreen.class)
-public class ClothConfigScreenMixin
+public abstract class ClothConfigScreenMixin
 {
-    @Shadow
-    private Rectangle tabsRightBounds;
+    private boolean hasFinishedInitialization = false;
+
+    @Inject(method = "init", at = @At("RETURN"))
+    public void markFinishedInitialization(CallbackInfo ci) {
+        this.hasFinishedInitialization = true;
+    }
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    public void cancelRenderingPreInitialization(MatrixStack xx, int child, int onlyInnerTabBounds, float entry, CallbackInfo ci) {
-        if (this.tabsRightBounds == null) {
+    public void cancelRenderingBeforeInitializationFinishes(int child, int onlyInnerTabBounds, float entry, CallbackInfo ci) {
+        if (!this.hasFinishedInitialization) {
             ci.cancel();
         }
     }
