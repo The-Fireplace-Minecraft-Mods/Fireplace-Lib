f96cf17a1d73b621647c7402cab4adc83d895fb2
The_Fireplace <the.f1repl4ce@gmail.com>
2021-11-06 14:10:03 -0500

    Fix cloth config screen render arguments for 1.16+

--- src/main/java/dev/the_fireplace/lib/mixin/clothconfig/ClothConfigScreenMixin.java
+++ src/main/java/dev/the_fireplace/lib/mixin/clothconfig/ClothConfigScreenMixin.java
@@ -3,6 +3,7 @@ package dev.the_fireplace.lib.mixin.clothconfig;
 import me.shedaniel.clothconfig2.gui.ClothConfigScreen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,7 +21,7 @@ public abstract class ClothConfigScreenMixin
     }
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    public void cancelRenderingBeforeInitializationFinishes(int child, int onlyInnerTabBounds, float entry, CallbackInfo ci) {
+    public void cancelRenderingBeforeInitializationFinishes(MatrixStack xx, int child, int onlyInnerTabBounds, float entry, CallbackInfo ci) {
         if (!this.hasFinishedInitialization) {
             ci.cancel();
         }
