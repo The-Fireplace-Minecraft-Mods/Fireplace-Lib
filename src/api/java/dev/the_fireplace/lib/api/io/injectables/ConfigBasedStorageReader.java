0e1abd5e9df495566157e47180be3639c6ec4099
The_Fireplace <the.f1repl4ce@gmail.com>
2021-12-28 18:34:27 -0600

    Add way to get stored configs

--- src/api/java/dev/the_fireplace/lib/api/io/injectables/ConfigBasedStorageReader.java
+++ src/api/java/dev/the_fireplace/lib/api/io/injectables/ConfigBasedStorageReader.java
@@ -2,6 +2,11 @@ package dev.the_fireplace.lib.api.io.injectables;
 
 import dev.the_fireplace.lib.api.io.interfaces.ConfigBasedSerializable;
 
-public interface ConfigBasedStorageReader {
+import java.util.Iterator;
+
+public interface ConfigBasedStorageReader
+{
     void readTo(ConfigBasedSerializable readable);
+
+    Iterator<String> getStoredConfigs(String subfolder);
 }
