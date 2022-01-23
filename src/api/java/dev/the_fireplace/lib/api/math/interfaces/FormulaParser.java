aa7e9f62e70a868be0c700d4cbb72350bf8ccbbc
The_Fireplace <the.f1repl4ce@gmail.com>
2022-01-23 12:19:02 -0600

    Added a generic formula parser

---
+++ src/api/java/dev/the_fireplace/lib/api/math/interfaces/FormulaParser.java
@@ -0,0 +1,10 @@
+package dev.the_fireplace.lib.api.math.interfaces;
+
+import dev.the_fireplace.lib.api.math.exception.ParsingException;
+
+public interface FormulaParser
+{
+    FormulaParser setVariable(char variable, String formulaValue);
+    FormulaParser setVariable(char variable, double value);
+    double parseDouble() throws ParsingException;
+}
