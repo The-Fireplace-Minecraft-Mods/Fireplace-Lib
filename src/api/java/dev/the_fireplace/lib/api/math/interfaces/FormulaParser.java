e4e625b787f3ff0133f24848a9eccf11c40b0ad0
The_Fireplace <the.f1repl4ce@gmail.com>
2022-01-27 19:44:33 -0600

    Automated refactor

--- src/api/java/dev/the_fireplace/lib/api/math/interfaces/FormulaParser.java
+++ src/api/java/dev/the_fireplace/lib/api/math/interfaces/FormulaParser.java
@@ -5,6 +5,8 @@ import dev.the_fireplace.lib.api.math.exception.ParsingException;
 public interface FormulaParser
 {
     FormulaParser setVariable(char variable, String formulaValue);
+
     FormulaParser setVariable(char variable, double value);
+
     double parseDouble() throws ParsingException;
 }
