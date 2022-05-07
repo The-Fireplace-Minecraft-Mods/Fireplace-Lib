ccbba87e448992113ce2a0ca4832fdce2b2ab0ef
The_Fireplace <git@the-fireplace.dev>
2022-05-07 10:35:05 -0500

    Fix mod initialization on Forge

--- forge/src/main/java/dev/the_fireplace/lib/entrypoints/Forge.java
+++ forge/src/main/java/dev/the_fireplace/lib/entrypoints/Forge.java
@@ -1,7 +1,7 @@
 package dev.the_fireplace.lib.entrypoints;
 
+import dev.the_fireplace.annotateddi.api.DIEventBus;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -16,9 +16,9 @@ public final class Forge
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 
         // Register "entrypoints"
-        MinecraftForge.EVENT_BUS.register(new Main());
+        DIEventBus.BUS.register(new Main());
         DistExecutor.unsafeCallWhenOn(Dist.CLIENT, () -> () -> {
-            MinecraftForge.EVENT_BUS.register(new Client());
+            DIEventBus.BUS.register(new Client());
             return null;
         });
     }
