ac00ef785b5c0d9f4909ef15f01f4bafd98ff5fa
The_Fireplace <git@the-fireplace.dev>
2022-05-10 20:05:45 -0500

    Add missing modid to config screen init

--- fabric/src/main/java/dev/the_fireplace/lib/entrypoints/ConfigGui.java
+++ fabric/src/main/java/dev/the_fireplace/lib/entrypoints/ConfigGui.java
@@ -1,6 +1,7 @@
 package dev.the_fireplace.lib.entrypoints;
 
 import com.google.inject.Injector;
+import dev.the_fireplace.lib.FireplaceLibConstants;
 import dev.the_fireplace.lib.api.client.entrypoints.ConfigGuiEntrypoint;
 import dev.the_fireplace.lib.api.client.interfaces.ConfigGuiRegistry;
 import dev.the_fireplace.lib.config.FLConfigScreenFactory;
@@ -10,6 +11,6 @@ public final class ConfigGui implements ConfigGuiEntrypoint
     @Override
     public void registerConfigGuis(Injector injector, ConfigGuiRegistry configGuiRegistry) {
         FLConfigScreenFactory flConfigScreenFactory = injector.getInstance(FLConfigScreenFactory.class);
-        configGuiRegistry.register(, flConfigScreenFactory::getConfigScreen);
+        configGuiRegistry.register(FireplaceLibConstants.MODID, flConfigScreenFactory::getConfigScreen);
     }
 }
