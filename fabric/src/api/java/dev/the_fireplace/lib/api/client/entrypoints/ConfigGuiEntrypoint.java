ff5835e63bde3d042850cbefe5c24670e5206747
The_Fireplace <git@the-fireplace.dev>
2022-06-18 15:56:03 -0500

    Don't provide Fireplace Lib's injector with the config GUI entrypoint
    Each mod may want to use its own injector if it needs to inject something, so avoid providing FL's injector which may cause confusion or unexpected errors.

--- fabric/src/api/java/dev/the_fireplace/lib/api/client/entrypoints/ConfigGuiEntrypoint.java
+++ fabric/src/api/java/dev/the_fireplace/lib/api/client/entrypoints/ConfigGuiEntrypoint.java
@@ -1,6 +1,5 @@
 package dev.the_fireplace.lib.api.client.entrypoints;
 
-import com.google.inject.Injector;
 import dev.the_fireplace.lib.api.client.interfaces.ConfigGuiRegistry;
 
 /**
@@ -10,5 +9,5 @@ import dev.the_fireplace.lib.api.client.interfaces.ConfigGuiRegistry;
  */
 public interface ConfigGuiEntrypoint
 {
-    void registerConfigGuis(Injector injector, ConfigGuiRegistry configGuiRegistry);
+    void registerConfigGuis(ConfigGuiRegistry configGuiRegistry);
 }
