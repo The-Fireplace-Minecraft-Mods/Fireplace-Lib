777c1ce511da703cbaab398a517768dee02e14dd
The_Fireplace <git@the-fireplace.dev>
2022-06-18 19:21:33 -0500

    Update to use Annotated DI 4

--- fabric/src/main/java/dev/the_fireplace/lib/entrypoints/Main.java
+++ fabric/src/main/java/dev/the_fireplace/lib/entrypoints/Main.java
@@ -1,13 +1,13 @@
 package dev.the_fireplace.lib.entrypoints;
 
-import com.google.inject.Injector;
-import dev.the_fireplace.annotateddi.api.entrypoints.DIModInitializer;
+import dev.the_fireplace.lib.FireplaceLibConstants;
 import dev.the_fireplace.lib.init.FireplaceLibInitializer;
+import net.fabricmc.api.ModInitializer;
 
-public final class Main implements DIModInitializer
+public final class Main implements ModInitializer
 {
     @Override
-    public void onInitialize(Injector container) {
-        container.getInstance(FireplaceLibInitializer.class).init();
+    public void onInitialize() {
+        FireplaceLibConstants.getInjector().getInstance(FireplaceLibInitializer.class).init();
     }
 }
