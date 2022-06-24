781058efbc68841bb3ec997609bb3d13a2f82cc7
The_Fireplace <git@the-fireplace.dev>
2022-06-23 19:37:44 -0500

    [1.17.X] Fix Fabric cloth config mod id

--- common/src/main/java/dev/the_fireplace/lib/CompatModids.java
+++ common/src/main/java/dev/the_fireplace/lib/CompatModids.java
@@ -2,6 +2,6 @@ package dev.the_fireplace.lib;
 
 public final class CompatModids
 {
-    public static final String CLOTH_CONFIG_FABRIC = "cloth-config";
+    public static final String CLOTH_CONFIG_FABRIC = "cloth-config2";
     public static final String CLOTH_CONFIG_FORGE = "cloth_config";
 }
