c3c172b4785e8ace78d1816158bdb7907f0181b6
The_Fireplace <git@the-fireplace.dev>
2022-07-01 23:05:30 -0500

    Add a fallback lookup for language files, should deal with Forge translation issues

--- common/src/main/java/dev/the_fireplace/lib/domain/io/LoaderSpecificDirectories.java
+++ common/src/main/java/dev/the_fireplace/lib/domain/io/LoaderSpecificDirectories.java
@@ -1,8 +1,11 @@
 package dev.the_fireplace.lib.domain.io;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
 public interface LoaderSpecificDirectories
 {
     Path getConfigPath();
+
+    Optional<Path> getResource(String modId, String path);
 }
--- fabric/src/main/java/dev/the_fireplace/lib/io/FabricSpecificDirectories.java
+++ fabric/src/main/java/dev/the_fireplace/lib/io/FabricSpecificDirectories.java
@@ -3,8 +3,10 @@ package dev.the_fireplace.lib.io;
 import dev.the_fireplace.annotateddi.api.di.Implementation;
 import dev.the_fireplace.lib.domain.io.LoaderSpecificDirectories;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
 @Implementation
 public final class FabricSpecificDirectories implements LoaderSpecificDirectories
@@ -12,5 +14,15 @@ public final class FabricSpecificDirectories implements LoaderSpecificDirectorie
     @Override
     public Path getConfigPath() {
         return FabricLoader.getInstance().getConfigDir();
+    }
+
+    @Override
+    public Optional<Path> getResource(String modId, String path) {
+        Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(modId);
+        if (modContainer.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return modContainer.get().findPath(path);
     }
 }
