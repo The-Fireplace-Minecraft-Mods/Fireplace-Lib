0b3475ab79e74b1a8242597b701f400027159bcc
The_Fireplace <git@the-fireplace.dev>
2022-07-01 23:05:30 -0500

    Add a fallback lookup for language files

--- common/src/main/java/dev/the_fireplace/lib/chat/translation/LanguageMap.java
+++ common/src/main/java/dev/the_fireplace/lib/chat/translation/LanguageMap.java
@@ -4,16 +4,22 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.google.inject.Injector;
 import dev.the_fireplace.lib.FireplaceLibConstants;
 import dev.the_fireplace.lib.api.io.injectables.DirectoryResolver;
+import dev.the_fireplace.lib.domain.io.LoaderSpecificDirectories;
 import net.minecraft.util.GsonHelper;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.IllegalFormatException;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Pattern;
 
@@ -25,9 +31,15 @@ final class LanguageMap
     @SuppressWarnings("RegExpRedundantEscape")
     private static final Pattern NUMERIC_VARIABLE_PATTERN = Pattern.compile("%(\\d+\\$)?[\\d\\.]*[df]");
     private final Map<String, String> languageList = new ConcurrentHashMap<>();
+    private final LoaderSpecificDirectories loaderSpecificDirectories;
+    private final String modId;
 
     LanguageMap(String modid, String locale) {
-        String langDir = FireplaceLibConstants.getInjector().getInstance(DirectoryResolver.class).getLangDirectory(modid);
+        Injector injector = FireplaceLibConstants.getInjector();
+        DirectoryResolver directoryResolver = injector.getInstance(DirectoryResolver.class);
+        this.loaderSpecificDirectories = injector.getInstance(LoaderSpecificDirectories.class);
+        this.modId = modid;
+        String langDir = directoryResolver.getLangDirectory(modid);
         try {
             JsonElement jsonelement = getLangJsonElement(locale, langDir);
             JsonObject jsonobject = GsonHelper.convertToJsonObject(jsonelement, "strings");
@@ -44,19 +56,36 @@ final class LanguageMap
 
     private JsonElement getLangJsonElement(String locale, String langDir) throws IOException {
         String langJsonPath = langDir + locale + ".json";
-        InputStream inputstream = LanguageMap.class.getResourceAsStream(langJsonPath);
+        InputStream inputstream = getInputStream(langJsonPath);
         if (inputstream == null) {
             FireplaceLibConstants.getLogger().error("Invalid locale: {}, defaulting to en_us.", locale);
-            inputstream = LanguageMap.class.getResourceAsStream(langDir + "en_us.json");
+            String defaultLangDir = langDir + "en_us.json";
+            inputstream = getInputStream(defaultLangDir);
         }
         if (inputstream == null) {
             FireplaceLibConstants.getLogger().error("Unable to read language file in directory {}!", langDir);
-            FireplaceLibConstants.getLogger().error("Class loader: {}", LanguageMap.class.getClassLoader().toString());
             return new JsonObject();
         }
         JsonElement jsonelement = new Gson().fromJson(new InputStreamReader(inputstream, StandardCharsets.UTF_8), JsonElement.class);
         inputstream.close();
         return jsonelement;
+    }
+
+    @Nullable
+    private InputStream getInputStream(String langFileDirectory) {
+        InputStream inputstream = LanguageMap.class.getResourceAsStream(langFileDirectory);
+        if (inputstream == null) {
+            Optional<Path> resource = loaderSpecificDirectories.getResource(modId, langFileDirectory);
+            if (resource.isPresent()) {
+                try {
+                    inputstream = Files.newInputStream(resource.get());
+                } catch (IOException exception) {
+                    FireplaceLibConstants.getLogger().warn(String.format("Could not create input stream for resource %s", resource.get()), exception);
+                }
+            }
+        }
+
+        return inputstream;
     }
 
     String translateKeyFormat(String key, Object... format) {
