ca20a5f8bbe32034e8f343c6fb15e1296f01814d
The_Fireplace <git@the-fireplace.dev>
2024-07-06 18:40:59 -0500

    Create a command to test command registration and feedback, the help command builder, and server started lifecycle event

---
+++ playtest-common/src/main/java/dev/the_fireplace/playtest/command/RegisterCommands.java
@@ -0,0 +1,40 @@
+package dev.the_fireplace.playtest.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import dev.the_fireplace.lib.api.command.injectables.HelpCommandFactory;
+import dev.the_fireplace.playtest.PlaytestConstants;
+import dev.the_fireplace.playtest.command.commands.OpEchoCommand;
+//import dev.the_fireplace.playtest.command.commands.PingCommand;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import net.minecraft.commands.CommandSourceStack;
+
+@Singleton
+public final class RegisterCommands
+{
+    private final HelpCommandFactory helpCommandFactory;
+    //private final PingCommand pingCommand;
+    private final OpEchoCommand opEchoCommand;
+
+    @Inject
+    public RegisterCommands(
+        HelpCommandFactory helpCommandFactory,
+        //PingCommand pingCommand,
+        OpEchoCommand opEchoCommand
+    ) {
+        this.helpCommandFactory = helpCommandFactory;
+        //this.pingCommand = pingCommand;
+        this.opEchoCommand = opEchoCommand;
+    }
+
+    public void register(CommandDispatcher<CommandSourceStack> commandDispatcher) {
+        helpCommandFactory.create(
+            PlaytestConstants.MODID,
+            LiteralArgumentBuilder.literal("testhelp")
+        ).addCommands(
+            //pingCommand.register(commandDispatcher),
+            opEchoCommand.register(commandDispatcher)
+        ).register(commandDispatcher);
+    }
+}
