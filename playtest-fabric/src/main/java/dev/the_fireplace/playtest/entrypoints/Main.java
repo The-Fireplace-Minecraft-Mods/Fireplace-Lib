e057e2e1329a4d7561f899f96269c79cf3e13056
The_Fireplace <git@the-fireplace.dev>
2024-07-06 14:37:39 -0500

    Get the playtest to run on Fabric

--- playtest-fabric/src/main/java/dev/the_fireplace/playtest/entrypoints/Main.java
+++ playtest-fabric/src/main/java/dev/the_fireplace/playtest/entrypoints/Main.java
@@ -1,13 +1,13 @@
 package dev.the_fireplace.playtest.entrypoints;
 
 import dev.the_fireplace.playtest.PlaytestConstants;
-import dev.the_fireplace.playtest.init.PlaytestInitializer;
+import dev.the_fireplace.playtest.init.IPlaytestInitializer;
 import net.fabricmc.api.ModInitializer;
 
 public final class Main implements ModInitializer
 {
     @Override
     public void onInitialize() {
-        PlaytestConstants.getInjector().getInstance(PlaytestInitializer.class).init();
+        PlaytestConstants.getInjector().getInstance(IPlaytestInitializer.class).init();
     }
 }
