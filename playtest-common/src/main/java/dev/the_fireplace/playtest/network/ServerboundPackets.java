e85a8db6b4986cfe725a8c6921ee9ab5e0ecc4e0
The_Fireplace <git@the-fireplace.dev>
2024-07-06 19:36:44 -0500

    Create a network testing command

---
+++ playtest-common/src/main/java/dev/the_fireplace/playtest/network/ServerboundPackets.java
@@ -0,0 +1,31 @@
+package dev.the_fireplace.playtest.network;
+
+import dev.the_fireplace.lib.api.network.interfaces.ServerboundPacketSpecification;
+import dev.the_fireplace.playtest.PlaytestConstants;
+import dev.the_fireplace.playtest.network.serverbound.SecondPing;
+import dev.the_fireplace.playtest.network.serverbound.ThirdPing;
+import jakarta.inject.Inject;
+import net.minecraft.resources.ResourceLocation;
+
+public final class ServerboundPackets
+{
+    public static final ResourceLocation SECOND_PING = new ResourceLocation(PlaytestConstants.MODID, "second_ping");
+    public static final ResourceLocation THIRD_PING = new ResourceLocation(PlaytestConstants.MODID, "third_ping");
+
+    private final ServerboundPacketSpecification secondPingSpec;
+    private final ServerboundPacketSpecification thirdPingSpec;
+
+    @Inject
+    public ServerboundPackets(SecondPing secondPingSpec, ThirdPing thirdPingSpec) {
+        this.secondPingSpec = secondPingSpec;
+        this.thirdPingSpec = thirdPingSpec;
+    }
+
+    public ServerboundPacketSpecification getSecondPingSpec() {
+        return secondPingSpec;
+    }
+
+    public ServerboundPacketSpecification getThirdPingSpec() {
+        return thirdPingSpec;
+    }
+}
---
+++ playtest-common/src/main/java/dev/the_fireplace/playtest/network/serverbound/SecondPing.java
@@ -0,0 +1,23 @@
+package dev.the_fireplace.playtest.network.serverbound;
+
+import dev.the_fireplace.lib.api.network.interfaces.ServerboundPacketReceiver;
+import dev.the_fireplace.lib.api.network.interfaces.ServerboundPacketSpecification;
+import dev.the_fireplace.playtest.PlaytestConstants;
+import dev.the_fireplace.playtest.network.ServerboundPackets;
+import dev.the_fireplace.playtest.network.serverbound.reciever.SecondPingReceiver;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.Supplier;
+
+public final class SecondPing implements ServerboundPacketSpecification
+{
+    @Override
+    public Supplier<ServerboundPacketReceiver> getReceiverFactory() {
+        return () -> PlaytestConstants.getInjector().getInstance(SecondPingReceiver.class);
+    }
+
+    @Override
+    public ResourceLocation getPacketID() {
+        return ServerboundPackets.SECOND_PING;
+    }
+}
