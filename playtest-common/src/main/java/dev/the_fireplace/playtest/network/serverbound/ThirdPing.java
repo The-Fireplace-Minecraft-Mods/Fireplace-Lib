17b9f4a6595a7c5ef401bea2aa0e3ee1795de0f7
The_Fireplace <git@the-fireplace.dev>
2024-07-06 19:36:44 -0500

    Create a network testing command

---
+++ playtest-common/src/main/java/dev/the_fireplace/playtest/network/serverbound/ThirdPing.java
@@ -0,0 +1,23 @@
+package dev.the_fireplace.playtest.network.serverbound;
+
+import dev.the_fireplace.lib.api.network.interfaces.ServerboundPacketReceiver;
+import dev.the_fireplace.lib.api.network.interfaces.ServerboundPacketSpecification;
+import dev.the_fireplace.playtest.PlaytestConstants;
+import dev.the_fireplace.playtest.network.ServerboundPackets;
+import dev.the_fireplace.playtest.network.serverbound.reciever.ThirdPingReceiver;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.Supplier;
+
+public final class ThirdPing implements ServerboundPacketSpecification
+{
+    @Override
+    public Supplier<ServerboundPacketReceiver> getReceiverFactory() {
+        return () -> PlaytestConstants.getInjector().getInstance(ThirdPingReceiver.class);
+    }
+
+    @Override
+    public ResourceLocation getPacketID() {
+        return ServerboundPackets.THIRD_PING;
+    }
+}
