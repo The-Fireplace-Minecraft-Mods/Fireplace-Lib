31cbb3f3e4745e330e2c93c9471f9f52279e428d
The_Fireplace <git@the-fireplace.dev>
2025-06-07 09:48:29 -0500

    [1.20.6] Fully drop unsupported Forge files

--- forge/src/api/java/dev/the_fireplace/lib/api/events/FLEventBus.java
+++
@@ -1,9 +0,0 @@
-package dev.the_fireplace.lib.api.events;
-
-import net.minecraftforge.eventbus.api.BusBuilder;
-import net.minecraftforge.eventbus.api.IEventBus;
-
-public final class FLEventBus
-{
-    public static final IEventBus BUS = BusBuilder.builder().build();
-}
